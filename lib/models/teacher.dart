3bb7928d013442255e4b25706b0554307830950c
P1er0VL <u20211a620@upc.edu.pe>
2025-10-06 15:41:42 -0500

    update: change teacher model

--- lib/models/teacher.dart
+++ lib/models/teacher.dart
@@ -1,29 +1,29 @@
-// lib/models/teacher.dart
 class Teacher {
-  final int id;
+  final int? id;
   final String firstName;
   final String lastName;
   final String email;
   final String dni;
   final String address;
   final String phone;
+  final int administratorId;
   final String username;
+  final String password;
 
   Teacher({
-    required this.id,
+    this.id,
     required this.firstName,
     required this.lastName,
     required this.email,
     required this.dni,
     required this.address,
     required this.phone,
+    required this.administratorId,
     required this.username,
+    required this.password,
   });
 
   factory Teacher.fromJson(Map<String, dynamic> json) {
-    if (json['id'] == null) {
-      throw Exception('El campo id es nulo en Teacher');
-    }
     return Teacher(
       id: json['id'],
       firstName: json['firstName'] ?? '',
@@ -32,20 +32,24 @@ class Teacher {
       dni: json['dni'] ?? '',
       address: json['address'] ?? '',
       phone: json['phone'] ?? '',
+      administratorId: json['administratorId'] ?? 0,
       username: json['username'] ?? '',
+      password: json['password'] ?? '',
     );
   }
 
   Map<String, dynamic> toJson() {
     return {
-      'id': id,
+      if (id != null) 'id': id,
       'firstName': firstName,
       'lastName': lastName,
       'email': email,
       'dni': dni,
       'address': address,
       'phone': phone,
+      'administratorId': administratorId,
       'username': username,
+      'password': password,
     };
   }
 }
