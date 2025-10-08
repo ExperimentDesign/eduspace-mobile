8fd988f78ba3844a496a34beaa95f7f61b778f72
P1er0VL <u20211a620@upc.edu.pe>
2025-10-08 17:54:29 -0500

    feat: add functionality to summary page

--- lib/views/summary/SummaryPage.dart
+++ lib/views/summary/SummaryPage.dart
@@ -4,7 +4,8 @@ import '../../services/classroom_service.dart';
 import '../../services/meetings_service.dart';
 import '../../models/classroom.dart';
 import '../../models/meeting.dart';
-import '../../widgets/app_drawer.dart';
+import '../../utils/token_utils.dart';
+import '../classroom-reports/TeacherClassroomPage.dart';
 
 class SummaryPage extends StatefulWidget {
   const SummaryPage({super.key});
@@ -23,8 +24,20 @@ class _SummaryPageState extends State<SummaryPage> {
   @override
   void initState() {
     super.initState();
-    _classroomsFuture = _classroomService.getAllClassrooms();
-    _meetingsFuture = _meetingsService.getAllMeetings();
+    _loadDataForTeacher();
+  }
+
+  void _loadDataForTeacher() async {
+    final profileId = await getProfileIdFromPrefs();
+    print('Using profileId for teacher: $profileId');
+    setState(() {
+      _classroomsFuture = profileId != null
+          ? _classroomService.getClassroomsByTeacherId(profileId)
+          : Future.value([]);
+      _meetingsFuture = profileId != null
+          ? _meetingsService.getAllMeetingsByTeacherId(profileId)
+          : Future.value([]);
+    });
   }
 
   @override
@@ -50,7 +63,8 @@ class _SummaryPageState extends State<SummaryPage> {
           child: ListView(
             padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
             children: [
-              const Text('Salones', style: TextStyle(color: Color(0xFF1976D2), fontWeight: FontWeight.bold, fontSize: 22)),
+              const Text('Salones asignados', style: TextStyle(color: Color(
+                  0xFF000000), fontWeight: FontWeight.bold, fontSize: 22)),
               const SizedBox(height: 10),
               FutureBuilder<List<Classroom>>(
                 future: _classroomsFuture,
@@ -64,21 +78,31 @@ class _SummaryPageState extends State<SummaryPage> {
                   } else {
                     final classrooms = snapshot.data!;
                     return Column(
-                      children: classrooms.map((classroom) => Card(
-                        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(18)),
-                        elevation: 6,
-                        margin: const EdgeInsets.symmetric(vertical: 8),
-                        child: ListTile(
-                          contentPadding: const EdgeInsets.symmetric(horizontal: 20, vertical: 12),
-                          title: Text(
-                            classroom.name,
-                            style: const TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 20,
-                              color: Color(0xFF1976D2),
+                      children: classrooms.map((classroom) => GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => TeacherClassroomPage(classroom: classroom),
                             ),
+                          );
+                        },
+                        child: Card(
+                          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(18)),
+                          elevation: 6,
+                          margin: const EdgeInsets.symmetric(vertical: 8),
+                          child: ListTile(
+                            contentPadding: const EdgeInsets.symmetric(horizontal: 20, vertical: 12),
+                            title: Text(
+                              classroom.name,
+                              style: const TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20,
+                                color: Color(0xFF1976D2),
+                              ),
+                            ),
+                            subtitle: Text(classroom.description, style: const TextStyle(fontSize: 16)),
                           ),
-                          subtitle: Text(classroom.description, style: const TextStyle(fontSize: 16)),
                         ),
                       )).toList(),
                     );
@@ -86,7 +110,8 @@ class _SummaryPageState extends State<SummaryPage> {
                 },
               ),
               const SizedBox(height: 24),
-              const Text('Reuniones', style: TextStyle(color: Color(0xFF1976D2), fontWeight: FontWeight.bold, fontSize: 22)),
+              const Text('Reuniones asignadas', style: TextStyle(color: Color(
+                  0xFF000000), fontWeight: FontWeight.bold, fontSize: 22)),
               const SizedBox(height: 10),
               FutureBuilder<List<Meeting>>(
                 future: _meetingsFuture,
